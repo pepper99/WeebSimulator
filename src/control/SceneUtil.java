b526636eae8c0a94d8577d36ab2c4c2106af8d00
phu1650 <phu.jong1@gmail.com>
2020-04-05 04:38:51 +0700

    delete comment

--- src/control/SceneUtil.java
+++ src/control/SceneUtil.java
@@ -26,7 +26,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-//import sun.tools.tree.ThisExpression;
+
 
 public class SceneUtil implements Commons {
 	
